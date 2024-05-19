7b91c5715fe42f4f7c44b9bb5e91ade4db0e6996
godoimatheus <matheus.godoi@live.com>
2024-05-19 17:47:58 -0300

    fix lint

--- test-pd.py
+++ test-pd.py
@@ -1,11 +1,8 @@
 import pandas as pd
 
-data = {
-  "calories": [420, 380, 390],
-  "duration": [50, 40, 45]
-}
+data = {'calories': [420, 380, 390], 'duration': [50, 40, 45]}
 
-df = pd.DataFrame(data, index=["day1", "day2", "day3"])
+df = pd.DataFrame(data, index=['day1', 'day2', 'day3'])
 
 for i, row in df.iterrows():
     # print(row['calories'], row['duration'])
